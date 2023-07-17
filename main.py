85925b36c22f929f490b16e258f6c6338aae5fbe
ShajidJeshan <shajidjeshan@gmail.com>
2023-07-17 19:16:52 +0600

    feat(API): added power calculation as power = voltage * current in GET API

--- main.py
+++ main.py
@@ -50,7 +50,31 @@ def get_data():
         for reading in readings
     ]
 
-    return jsonify({"success": True, "data": data}), 200
+    average_power = {}
+
+    for reading in readings:
+        date = str(reading.timestamp.date())
+        key = reading.name
+        volt = int(key == "Voltage")
+        current = int(key == "Current")
+
+        average_power.setdefault(date, {}).setdefault(key, 0.0)
+        average_power.setdefault(date, {}).setdefault("volt_count", 0)
+        average_power.setdefault(date, {}).setdefault("current_count", 0)
+        average_power[date][key] += round(reading.value, 2)
+        average_power[date]["volt_count"] += volt
+        average_power[date]["current_count"] += current
+
+    avg_power_per_day = [
+        {
+            "date": key,
+            "average_power": (average_power[key].get("Voltage", 0) / (1 if average_power[key].get("volt_count", 0) == 0 else average_power[key].get("volt_count", 0))) *
+                             (average_power[key].get("Current", 0) / (1 if average_power[key].get("current_count", 0) == 0 else average_power[key].get("current_count", 0)))
+        }
+        for key in average_power
+    ]
+
+    return jsonify({"success": True, "data": data, "avg_power_per_day": avg_power_per_day}), 200
 
 
 if __name__ == "__main__":
